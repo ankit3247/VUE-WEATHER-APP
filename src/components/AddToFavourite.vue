ecc54743013a2bacde9dec623d19991fe44ce70f
ankit3247 <ankitupadhyay441@gmail.com>
2021-10-04 17:01:06 +0530

    api favourite section

---
+++ src/components/AddToFavourite.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>Add To Favourite City</div>
+</template>
+
+<script>
+export default {
+    name:'Add to Favourite City',
+     data () {
+    return {
+      api_key:'e71c2b2a7d6700a136b1ccc4cb362cc1',
+      url_base:'https://api.openweathermap.org/data/2.5/',
+      query:'',
+      weather:{}
+    }
+
+  },
+  methods: {
+    fetchWeather (e) {
+      if(e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+
+
+          }).then(this.setResults);
+      }
+    },
+    FavouriteCity(){
+  
+    let city = ['Pune'];
+
+    console.log(city.length);
+    }
+  }
+
+
+
+}
+</script>
+
+<style>
+
+</style>
+
+//  test 12345
